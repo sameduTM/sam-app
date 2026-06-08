33e9c60b28c619fd110ddb6e989d1bb1541559bf
Ken Wekesa <wanyamak884@gmail.com>
2026-06-08 23:32:09 +0300

    update the lambda function to create a new version

--- users/app.py
+++ users/app.py
@@ -1,7 +1,10 @@
 import json
 
+"""Simple lambda function to create and fetch users from mock db"""
+
 
 def get_users(event, context):
+    """Fetch all users"""
     return {
         "statusCode": 200,
         "body": json.dumps({"Name": "Methuselah", "Age": 190}),
@@ -10,7 +13,8 @@ def get_users(event, context):
 
 
 def create_user(event, context):
+    """Test POST request by creating a user"""
     user_object = event.get("body")
     user = user_object
-    user_name = json.loads(user).get('Name')
+    user_name = json.loads(user).get("Name")
     return {"statusCode": 201, "body": f"User {user_name} created successfully"}
